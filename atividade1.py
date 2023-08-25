23fda2d357649986dcd2b1133c8ddde6a264757e
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-08-25 14:40:55 -0400

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -27,6 +27,23 @@ def criar_vetor_quase_ordenado(vetor_ordenado, porcentagem_desordem):
     
     return vetor_quase_ordenado
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+
+
+
+
+
+
+#################################################EXECUÇÃO####################################################
 inc = int(input('Digite o valor de início do vetor: '))
 fim = int(input('Digite o valor final do vetor: '))
 stp = int(input('Digite o intervalo de soma entre as posições do vetor: '))
@@ -41,9 +58,8 @@ vetor_ordenado = criar_vetor_ordenado(inc, fim, stp)
 vetor_aleatorio = criar_vetor_aleatorio(vetor_ordenado)
 vetor_reverso = criar_vetor_reverso(inc, fim, stp)
 vetor_quase_ordenado = criar_vetor_quase_ordenado(vetor_ordenado, 0.2)  # Exemplo de 20% de desordem
-
-# Imprimir os vetores
-print('Vetor Ordenado:', vetor_ordenado)
-print('Vetor Aleatório:', vetor_aleatorio)
-print('Vetor Reverso:', vetor_reverso)
-print('Vetor Quase Ordenado:', vetor_quase_ordenado)
+# prints para ver os vetores
+# print('Vetor Ordenado:', vetor_ordenado)
+# print('Vetor Aleatório:', vetor_aleatorio)
+# print('Vetor Reverso:', vetor_reverso)
+# print('Vetor Quase Ordenado:', vetor_quase_ordenado)
