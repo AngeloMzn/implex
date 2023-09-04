fe4fb6c1860b3984f4e3fcc365398e22332ae3ba
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-09-04 15:31:01 -0400

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -105,7 +105,6 @@ def heapify(result, n, i):
     if largest != i:
         result[i], result[largest] = result[largest], result[i]
         heapify(result, n, largest)
-
 def heap_sort(vetor):
     result = vetor.copy()
     n = len(result)
@@ -133,17 +132,13 @@ def counting_sort(vetor):
     max_val = max(aux)
     count = [0] * (max_val + 1)
     result = [0] * len(aux)
-    
     for num in aux:
         count[num] += 1
-    
     for i in range(1, len(count)):
         count[i] += count[i - 1]
-    
     for num in aux:
         result[count[num] - 1] = num
         count[num] -= 1
-    
     return result
 rpt = int(input('Digite o número de vezes que o teste deve se repetir: '))
 inc = 0
